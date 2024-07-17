aa169b0cd049479dc8687b0e921c595d0fcfae95
gichukipitar <ptahgichuki@yahoo.com>
2024-07-17 18:42:19 +0300

    controller class updates

--- src/main/java/com/fleet/managament/accounts/controller/InvoiceController.java
+++ src/main/java/com/fleet/managament/accounts/controller/InvoiceController.java
@@ -1,9 +1,15 @@
 package com.fleet.managament.accounts.controller;
 
+import com.fleet.managament.accounts.services.InvoiceService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/accounts")
 public class InvoiceController {
+
+    private final InvoiceService invoiceService;
+    public InvoiceController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
 }
