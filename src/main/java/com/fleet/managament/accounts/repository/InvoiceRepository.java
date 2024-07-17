f7f496f945d00902d67b0c533fa78e092db0b1e8
gichukipitar <ptahgichuki@yahoo.com>
2024-07-17 18:30:14 +0300

    repository and service class

--- src/main/java/com/fleet/managament/accounts/repository/InvoiceRepository.java
+++ src/main/java/com/fleet/managament/accounts/repository/InvoiceRepository.java
@@ -1,5 +1,6 @@
 package com.fleet.managament.accounts.repository;
 
+import com.fleet.managament.accounts.entity.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvoiceRepository extends JpaRepository <Invoice, Long> {
--- src/main/java/com/fleet/managament/accounts/services/InvoiceService.java
+++ src/main/java/com/fleet/managament/accounts/services/InvoiceService.java
@@ -1,7 +1,16 @@
 package com.fleet.managament.accounts.services;
 
+import com.fleet.managament.accounts.repository.InvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class InvoiceService {
+    private InvoiceRepository invoiceRepository;
+    @Autowired
+    public InvoiceService(InvoiceRepository invoiceRepository) {
+        this.invoiceRepository = invoiceRepository;
+    }
+
+
 }
