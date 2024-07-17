70feff467a3658e711d1248c3d259da5a0d97a76
gichukipitar <ptahgichuki@yahoo.com>
2024-07-17 15:20:50 +0300

    Initial commit

---
+++ src/test/java/com/fleet/managament/ManagamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleet.managament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
