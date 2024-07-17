919117be060837d2b0c8016b08dc7261f466ec3d
gichukipitar <ptahgichuki@yahoo.com>
2024-07-17 17:31:29 +0300

    updates on the main class

---
+++ src/main/java/com/fleet/managament/FleetManagementApplication.java
@@ -0,0 +1,21 @@
+package com.fleet.managament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class FleetManagementApplication {
+
+	@Bean
+	public AuditorAware<String> auditorAware() {
+		return new SpringSecurityAuditorAware();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagementApplication.class, args);
+	}
+
+}
