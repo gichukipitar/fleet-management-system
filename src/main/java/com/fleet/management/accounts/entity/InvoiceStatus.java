021d62602e7d59eb8f0fd4a5e26cd323b7b7b590
gichukipitar <ptahgichuki@yahoo.com>
2024-07-18 15:56:49 +0300

    commonobject class

--- src/main/java/com/fleet/management/accounts/entity/InvoiceStatus.java
+++ src/main/java/com/fleet/management/accounts/entity/InvoiceStatus.java
@@ -2,6 +2,7 @@ package com.fleet.management.accounts.entity;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.fleet.management.parameters.entity.CommonObject;
 import jakarta.persistence.Entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
