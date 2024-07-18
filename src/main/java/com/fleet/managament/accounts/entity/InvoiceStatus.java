6ef45375b881538b05df74d8c950d1dc471d7902
gichukipitar <ptahgichuki@yahoo.com>
2024-07-18 15:33:07 +0300

    InvoiceStatus class

---
+++ src/main/java/com/fleet/managament/accounts/entity/InvoiceStatus.java
@@ -0,0 +1,15 @@
+package com.fleet.managament.accounts.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper=false)
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class InvoiceStatus extends CommonObject {
+
+}
