ed990ed791a2e1657e7ae3d54013929bf3b17868
gichukipitar <ptahgichuki@yahoo.com>
2024-07-26 17:51:11 +0300

    UserRepository

---
+++ src/main/java/com/fleet/management/security/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.fleet.management.security.repository;
+
+import com.fleet.management.security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
