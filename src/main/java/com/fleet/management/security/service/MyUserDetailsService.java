175cba75d62134df21bab16a67ef001c086652ee
gichukipitar <ptahgichuki@yahoo.com>
2024-07-27 11:02:34 +0300

    MyUserDetailsService class

---
+++ src/main/java/com/fleet/management/security/service/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.fleet.management.security.service;
+
+import com.fleet.management.security.entity.User;
+import com.fleet.management.security.entity.UserPrincipal;
+import com.fleet.management.security.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("user not found");
+        }
+        return new UserPrincipal(user);
+    }
+}
