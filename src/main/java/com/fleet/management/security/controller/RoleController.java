ef343b7bf298640c48df27cd3953dc65bf631e56
gichukipitar <ptahgichuki@yahoo.com>
2024-07-29 08:50:19 +0300

    RoleController

---
+++ src/main/java/com/fleet/management/security/controller/RoleController.java
@@ -0,0 +1,9 @@
+package com.fleet.management.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class RoleController {
+}
