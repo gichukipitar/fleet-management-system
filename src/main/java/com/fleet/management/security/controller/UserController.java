07fe636df5762ba3481869db624fa9f9d89489b0
gichukipitar <ptahgichuki@yahoo.com>
2024-07-30 10:41:02 +0300

    User Controller

---
+++ src/main/java/com/fleet/management/security/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.fleet.management.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/security")
+public class UserController {
+
+}
