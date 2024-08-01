33d702f405c9160120a1acf307b75a7e360931ec
gichukipitar <ptahgichuki@yahoo.com>
2024-08-01 18:31:49 +0300

    RestResponse

---
+++ src/main/java/com/fleet/management/utils/RestResponse.java
@@ -0,0 +1,10 @@
+package com.fleet.management.utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class RestResponse extends ResponseEntity <RestResponseObject> {
+    public RestResponse(RestResponseObject body, HttpStatus statusCode) {
+        super(body, statusCode);
+    }
+}
