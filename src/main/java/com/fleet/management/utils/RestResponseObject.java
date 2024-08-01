462fe776c05390de117599f00a72283e0ff83013
gichukipitar <ptahgichuki@yahoo.com>
2024-08-01 18:16:07 +0300

    RestResponseObject class

---
+++ src/main/java/com/fleet/management/utils/RestResponseObject.java
@@ -0,0 +1,14 @@
+package com.fleet.management.utils;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class RestResponseObject <T> {
+    private String message;
+    private T payload;
+    private List<ErrorMessage> errors;
+}
