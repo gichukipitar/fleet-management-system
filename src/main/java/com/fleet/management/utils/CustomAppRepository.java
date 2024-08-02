be830377468b74f4f8a215a74073b269684b9d99
gichukipitar <ptahgichuki@yahoo.com>
2024-08-02 09:44:27 +0300

    custom app repositoy

--- src/main/java/com/fleet/management/utils/CustomAppRepository.java
+++ src/main/java/com/fleet/management/utils/CustomAppRepository.java
@@ -1,5 +1,14 @@
 package com.fleet.management.utils;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 public interface CustomAppRepository <T> {
+    List<T> findByFieldAndValue(Class <T> entityClass, String fieldName, String value);
+    Page<T> findByQueryWithPagination(Class <T> entityClass, Query query, PageRequest pageRequest);
 
 }
