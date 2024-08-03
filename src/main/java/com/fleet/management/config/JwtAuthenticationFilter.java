e059ce7f2e538bbbac4b685475e22ac961f27d69
gichukipitar <ptahgichuki@yahoo.com>
2024-08-03 10:58:30 +0300

    JwtAuthenticationFilter

---
+++ src/main/java/com/fleet/management/config/JwtAuthenticationFilter.java
@@ -0,0 +1,11 @@
+package com.fleet.management.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+}
