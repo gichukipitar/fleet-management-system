a0a68fbc2b383441caca9a82067d18d5d376e7e0
gichukipitar <ptahgichuki@yahoo.com>
2024-08-05 09:05:53 +0300

    jwt updates

--- src/main/java/com/fleet/management/security/service/JwtService.java
+++ src/main/java/com/fleet/management/security/service/JwtService.java
@@ -40,10 +40,14 @@ public class JwtService {
     }
 
     private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
-        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000* 60 * 60 *10))
+                .signWith(getSigningKey(), Jwts.SIG.HS256)
+                .compact();
     }
 
     private boolean isTokenExpired(String token) {
